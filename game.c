c3c74eece978f5b41a559bb12b9a1d6b170f0d67
bleachchyxo <85224206+bleachchyxo@users.noreply.github.com>
2022-10-08 17:35:25 -0500

    Create game.c

---
+++ game.c
@@ -0,0 +1,122 @@
+#include <termios.h>
+#include <stdio.h>
+
+static struct termios old, new;
+
+/* Initialize new terminal i/o settings */
+void initTermios(int echo){
+        tcgetattr(0, &old); //grab old terminal i/o settings
+        new = old; //make new settings same as old settings
+        new.c_lflag &= ~ICANON; //disable buffered i/o
+        new.c_lflag &= echo ? ECHO : ~ECHO; //set echo mode
+        tcsetattr(0, TCSANOW, &new); //apply terminal io settings
+}
+
+/* Restore old terminal i/o settings */
+void resetTermios(void){
+        tcsetattr(0, TCSANOW, &old);
+}
+
+/* Read 1 character - echo defines echo mode */
+char getch_(int echo){
+        char ch;
+        initTermios(echo);
+        ch = getchar();
+        resetTermios();
+        return ch;
+}
+
+/*
+ * Read 1 character without echo
+ * getch() function definition.
+ */
+char getch(void){
+        return getch_(0);
+}
+
+/*
+ * Read 1 character with echo
+ * getche() function definition.
+ */
+char getche(void){
+        return getch_(1);
+}
+
+int main(void){
+        char c;
+        char cong[15] = ">Continue Game";
+        char newg[10] = " New Game";
+        char exitg[6] = " Exit";
+        int mainmenuloop = 1;
+        int mainmenu = 3;
+        int menuopt;
+
+        while (mainmenuloop == 1){
+
+        system("clear");
+        printf("%s\n", cong);
+        printf("%s\n", newg);
+        printf("%s\n", exitg);
+
+        /* Choosen option */
+        menuopt = mainmenu;
+
+        c = getche();
+
+        if(c == 'w'){
+                mainmenu++;
+        }
+        else if(c == 's'){
+                mainmenu--;
+        }
+        else if(c == 'k'){
+                mainmenu = 10;
+        }
+        else{
+                printf("Not valid key\n");
+        }
+
+        switch(mainmenu){
+            case 10:
+                        mainmenuloop = 2;
+                        break;
+                case 4:
+                        mainmenu = 3;
+                        break;
+                case 0:
+                        mainmenu = 1;
+                        break;
+                case 3:
+                        cong[0] = '>';
+                        newg[0] = ' ';
+                        exitg[0] = ' ';
+                        break;
+                case 2:
+                        cong[0] = ' ';
+                        newg[0] = '>';
+                        exitg[0] = ' ';
+                        break;
+                case 1:
+                        cong[0] = ' ';
+                        newg[0] = ' ';
+                        exitg[0] = '>';
+                        break;
+        }
+
+        }
+
+        if(menuopt == 3){
+                system("clear");
+                printf("Playing\n");
+                FILE *fp;
+                fp = fopen ("data.txt", "w");
+        }
+        else if(menuopt == 2){
+                printf("new game\n");
+        }
+        else if(menuopt == 1){
+                printf("exit\n");
+        }
+
+        return 0;
+}
